2deb3654550f96d36a281d0733ee1421f1d652f6
Travis Durrant <kinglink89@yahoo.com>
2023-02-22 12:56:48 -0700

    adding js for active

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,18 @@
-// main.js - mainly for buttons and event listeners
+// main.js
 
+const navLi = document.querySelectorAll('nav div ul li a');
+const sections = document.querySelectorAll('section');
 
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        let sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 1) {
+            current = section.getAttribute('id');
+        }
+    });
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        document.querySelector('nav div ul li a[href*= ' + current + ']').classList.add('active');
+    });
+});
