c71f7beadc7ea4b2a0454e317ed751142bc161aa
Travis Durrant <kinglink89@yahoo.com>
2024-06-26 16:45:24 -0600

    changes to active section

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,22 @@
 // main.js
 
+
 const navLi = document.querySelectorAll('nav div ul li a');
 const sections = document.querySelectorAll('section');
 
 window.addEventListener('scroll', () => {
     let current = '';
     sections.forEach(section => {
-        let sectionTop = section.offsetTop;
-        if (scrollY >= sectionTop - 1) {
+        let sectionTop = section.offsetTop - 5;
+        if (scrollY >= sectionTop) {
             current = section.getAttribute('id');
         }
     });
     navLi.forEach(li => {
         li.classList.remove('active');
-        document.querySelector('nav div ul li a[href*= ' + current + ']').classList.add('active');
+        //document.querySelector('nav div ul li a[href*= ' + current + ']').classList.add('active');
+        if (li.getAttribute('href').includes(current)) {
+            li.classList.add('active');
+        }
     });
 });
