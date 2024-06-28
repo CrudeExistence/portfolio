f611fbd7852820541c45c75a4cf284e986579a3e
CrudeExistence <kinglink89@yahoo.com>
2024-06-28 17:36:56 -0600

    tweaks to narrow down issue

--- assets/main.js
+++ assets/main.js
@@ -2,19 +2,22 @@
 
 
 const navLi = document.querySelectorAll('nav div ul li a');
-const sections = document.querySelectorAll('section');
+const sections = document.querySelectorAll('#wrapper #main section');
 
 window.addEventListener('scroll', () => {
     let current = '';
     sections.forEach(section => {
+        // Adjustment to offset the navigation bar at the top of the screen.
         let sectionTop = section.offsetTop - 5;
-        if (scrollY >= sectionTop) {
+        let sectionHeight = section.offsetHeight;
+        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
             current = section.getAttribute('id');
         }
     });
     navLi.forEach(li => {
+        // First make sure that all list items have the active class removed
         li.classList.remove('active');
-        //document.querySelector('nav div ul li a[href*= ' + current + ']').classList.add('active');
+        // Then add the active class to the current section
         if (li.getAttribute('href').includes(current)) {
             li.classList.add('active');
         }
